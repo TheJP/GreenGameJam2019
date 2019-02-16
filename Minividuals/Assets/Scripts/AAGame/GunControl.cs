25460b112668e8d2423b88f9b10c979fcb6b61c9
Florian Graf <floriangraf@gmx.ch>
2019-02-16 12:05:01 +0100

    Added possibility to set the color of the canon

--- Minividuals/Assets/Scripts/AAGame/GunControl.cs
+++ Minividuals/Assets/Scripts/AAGame/GunControl.cs
@@ -16,7 +16,27 @@ namespace AAGame
 
         [SerializeField]
         private Transform canon;
+
+        private Color color;
         
+        public Color Color
+        {
+            get => color;
+            set
+            {
+                color = value;
+                foreach(var meshRenderer in GetComponentsInChildren<MeshRenderer>())
+                {
+                    meshRenderer.material.color = value;
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            Color = Color.green;
+        }
+
         private void Update()
         {
             var x = Input.GetAxis("Horizontal") * horizontalCursorSpeed * Time.deltaTime;
