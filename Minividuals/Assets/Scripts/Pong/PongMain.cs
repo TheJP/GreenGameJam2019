2f4de99d1f079d2493e7da5c8e1db14845696a41
Stefan Winterberger <stefan.winterberger@ihomelab.ch>
2019-02-16 16:05:33 +0100

    Moved FadeOutText prefab a layer up. Added correct controls to the pong minigames.

--- Minividuals/Assets/Scripts/Pong/PongMain.cs
+++ Minividuals/Assets/Scripts/Pong/PongMain.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using Assets.Scripts.Board;
+using UnityEngine;
 
 public class PongMain : MonoBehaviour
 {
@@ -6,34 +9,49 @@ public class PongMain : MonoBehaviour
     [SerializeField] private GameObject pongBallPrefab;
     [SerializeField] private GameObject pongPlayBoardPrefab;
     [SerializeField] private GameObject pongPlayerPrefab;
+    [SerializeField] private GameObject countdownUiPrefab;
+
+    //Local Settings in case no main game is running.
     [SerializeField] private Color[] playerColors;
+    [SerializeField] private String[] controlPrefixes;
 #pragma warning restore 649
 
     private const int TimeTillStart = 3;
     private const int MinPlayerNumber = 1;
-    private const int MaxPlayerNumber = 2;
+    private const int MaxPlayerNumber = 4;
+
+    private PongPlayboard pongPlayboard;
+    private BoardController boardController;
 
     // Start is called before the first frame update
     void Start()
     {
         var playBoardObject = Instantiate(pongPlayBoardPrefab, transform);
-        PongPlayboard pongPlayboard = playBoardObject.GetComponent<PongPlayboard>();
+        pongPlayboard = playBoardObject.GetComponent<PongPlayboard>();
         pongPlayboard.MinPlayerNumber = MinPlayerNumber;
         pongPlayboard.MaxPlayerNumber = MaxPlayerNumber;
 
         var pongBallObject = Instantiate(pongBallPrefab, transform);
         PongBall pongBall = pongBallObject.GetComponent<PongBall>();
 
-        for (int playerNumber = MinPlayerNumber; playerNumber <= MaxPlayerNumber; playerNumber++)
+        boardController = GameObject.Find("BoardController")?.GetComponent<BoardController>();
+        InstantiatePlayers();
+
+        StartCoroutine(CountDownForStart(TimeTillStart));
+    }
+
+    private void InstantiatePlayers()
+    {
+        int maxPlayerNumber = boardController != null ? boardController.players.Players.Count : MaxPlayerNumber;
+        for (int playerNumber = 1; playerNumber <= maxPlayerNumber; playerNumber++)
         {
-            InstantiatePlayer(pongPlayboard, playerNumber);
+            InstantiatePlayer(playerNumber);
         }
     }
 
-
-    private void InstantiatePlayer(PongPlayboard playBoard, int playerNumber)
+    private void InstantiatePlayer(int playerNumber)
     {
-        var spawnTransform = playBoard.GetSpawnPointForPlayer(playerNumber);
+        var spawnTransform = pongPlayboard.GetSpawnPointForPlayer(playerNumber);
         GameObject playerObject = Instantiate(pongPlayerPrefab, spawnTransform.position,
             spawnTransform.rotation, transform);
 
@@ -42,9 +60,41 @@ public class PongMain : MonoBehaviour
 
         PongPlayerControl playerControl = playerObject.GetComponent<PongPlayerControl>();
         playerControl.SetPlayerNumber(playerNumber);
-//        playerControl.enabled = false;
+        playerControl.ControlPrefix = boardController != null
+            ? boardController.players.Players[playerNumber].InputPrefix
+            : controlPrefixes[playerNumber - 1];
+        playerControl.enabled = false;
 
         PongPlayerStyle playerStyle = playerObject.GetComponent<PongPlayerStyle>();
-        playerStyle.SetColor(playerColors[playerNumber-1]);
+        playerStyle.SetColor(boardController != null
+            ? boardController.players.Players[playerNumber].Colour
+            : playerColors[playerNumber - 1]);
+    }
+
+    private IEnumerator CountDownForStart(float startTime)
+    {
+        float currentTime = startTime;
+        while (currentTime >= 0)
+        {
+            var fadeoutObject = Instantiate(countdownUiPrefab);
+            var countdownScript = fadeoutObject.GetComponent<Countdown>();
+            countdownScript.TextMesh.text = Math.Ceiling(currentTime).ToString();
+            countdownScript.TextMesh.color = new Color(0.6392157F, 0.5019608F, 0.3892157F, 1.0F);
+
+            currentTime -= 1;
+            yield return new WaitForSeconds(1);
+        }
+
+        EnableControls();
+    }
+
+    private void EnableControls()
+    {
+        var players = GetComponentsInChildren<PongPlayer>();
+
+        foreach (var player in players)
+        {
+            player.GetComponent<PongPlayerControl>().enabled = true;
+        }
     }
 }
--- Minividuals/Assets/Scripts/Pong/PongPlayerControl.cs
+++ Minividuals/Assets/Scripts/Pong/PongPlayerControl.cs
@@ -7,9 +7,11 @@ public class PongPlayerControl : MonoBehaviour
     [SerializeField] private int playerNumber;
 #pragma warning restore 649
 
+    public string ControlPrefix { get; set; }
+
     private void Update()
     {
-        float moveHorizontal = Input.GetAxis($"Player{playerNumber}_Horizontal");
+        float moveHorizontal = Input.GetAxis($"{ControlPrefix}Horizontal");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
         transform.Translate(movement * Time.deltaTime * speed);
     }
