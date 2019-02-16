2a02c523f4e2e08e114b4b25c45c9c2e3da01064
Edith Birrer <edith@edith-birrer.ch>
2019-02-16 21:03:40 +0100

    fixed missing Playboard

--- Minividuals/Assets/Scripts/MelodyMemory/MeloMemoMain.cs
+++ Minividuals/Assets/Scripts/MelodyMemory/MeloMemoMain.cs
@@ -7,44 +7,28 @@ namespace MelodyMemory
     public class MeloMemoMain : MonoBehaviour
     {
 #pragma warning disable 649
-        [SerializeField] private GameObject playboardPrefab;
         [SerializeField] private GameObject tilePrefab;
 #pragma warning restore 649
 
         // Start is called before the first frame update
         private void Start()
         {
-            GameObject playboardObject = Instantiate(playboardPrefab);
-            Playboard tile = playboardObject.GetComponent<Playboard>();
-            for (int tileNumber = 1; tileNumber <= 32; tileNumber++)
+            GameObject tileObject = Instantiate(tilePrefab);
+            ColorSoundTile tile = tileObject.GetComponent<ColorSoundTile>();
+            for (int tileNumber = 0; tileNumber < 32; tileNumber++)
             {
                 InstantiateTile(tile, tileNumber);
             }
-
-            //Start Game... Enable Controls...
-
-        
-            GameObject tileObject = Instantiate(tilePrefab);        // TODO we need an array of these
-            // TODO init tiles
-            Playboard playboard = tileObject.GetComponent<Playboard>();
-            for (int playerNumber = 1; playerNumber <= 4; playerNumber++)
-            {
-                InstantiateTile(playboard, playerNumber);
-            }
-
-            //Start Game... Enable Controls...
         }
 
-        private void InstantiateTile(Playboard playboard, int tileNumber)
+        private void InstantiateTile(ColorSoundTile tile, int tileNumber)
         {
-            GameObject playerObject = Instantiate(tilePrefab, playboard.GetSpawnpointForPlayer(tileNumber),
-                Quaternion.identity);
+            
+//            PlayerControls playerControls = playerObject.GetComponent<PlayerControls>();
+//            playerControls.SetPlayerNumber(tileNumber);
 
-            PlayerControls playerControls = playerObject.GetComponent<PlayerControls>();
-            playerControls.SetPlayerNumber(tileNumber);
-
-            PlayerStyle playerStyle = playerObject.GetComponent<PlayerStyle>();
-            playerStyle.SetColor(playboard.GetColorForPlayer(tileNumber));
+//            PlayerStyle playerStyle = playerObject.GetComponent<PlayerStyle>();
+//            playerStyle.SetColor(playboard.GetColorForPlayer(tileNumber));
         }
     }
     
