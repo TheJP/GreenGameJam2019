b985fe665f35bddf5620871c46a8b7b0d145d2cd
TheJP <jp_55555@hotmail.com>
2019-02-16 04:23:21 +0100

    Added reveal animation to clouds

--- Minividuals/Assets/Scripts/Board/Tile.cs
+++ Minividuals/Assets/Scripts/Board/Tile.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
 
 namespace Assets.Scripts.Board
 {
@@ -10,13 +12,20 @@ namespace Assets.Scripts.Board
         [Tooltip("Clouds that hide the tile")]
         public SpriteRenderer[] clouds;
 
+        [Tooltip("Time in seconds until clouds disappear")]
+        public float cloudFadeOutDuration = 3f;
+
+        [Tooltip("Distance that the clouds are moved while fading out")]
+        public float cloudFadeOutDistance = 0.5f;
+
         public bool IsPlayerHome => Owner != null;
 
         public Player Owner { get; private set; }
 
         private void Start()
         {
-            foreach(var cloud in clouds)
+            // Add random rotation and translation to clouds, so that they don't look that uniform
+            foreach (var cloud in clouds)
             {
                 cloud.transform.Rotate(Vector3.forward, Random.Range(-15, 15));
                 cloud.transform.Translate(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0);
@@ -30,8 +39,27 @@ namespace Assets.Scripts.Board
             HideClouds();
         }
 
-        public void HideClouds()
+        public void HideClouds() => StartCoroutine(HideCloudsCoroutine());
+
+        private IEnumerator HideCloudsCoroutine()
         {
+            if (clouds.Length <= 0) { yield break; }
+
+            // Move clouds away from center and fade them out at the same time
+            var middle = clouds.Aggregate(Vector3.zero, (sum, cloud) => sum + cloud.transform.position) / clouds.Length;
+            var start = Time.time;
+            while (Time.time - start < cloudFadeOutDuration)
+            {
+                var distance = cloudFadeOutDistance / cloudFadeOutDuration * Time.deltaTime;
+                foreach (var cloud in clouds)
+                {
+                    cloud.transform.position += (cloud.transform.position - middle).normalized * distance;
+                    cloud.color = Color.Lerp(Color.white, new Color(1, 1, 1, 0), (Time.time - start) / cloudFadeOutDuration);
+                }
+                yield return null; // Wait for next frame (next Update)
+            }
+            
+            // Deactivate invisible clouds
             foreach (var cloud in clouds)
             {
                 cloud.gameObject.SetActive(false);
