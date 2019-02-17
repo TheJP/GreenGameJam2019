73260867dba61b69813116622fdbfdc0bcaa3ebe
Stefan Winterberger <stefan.winterberger@ihomelab.ch>
2019-02-17 10:45:36 +0100

    Increased light intensity in pong.

--- Minividuals/Assets/Scripts/Pong/PongMain.cs
+++ Minividuals/Assets/Scripts/Pong/PongMain.cs
@@ -94,7 +94,17 @@ public class PongMain : MonoBehaviour
         if (maxPlayerNumber == 1)
         {
             Debug.Log("You started a Pong with one player? Doesn't really make sense, huh?");
-            //TODO: Handle single player properly
+            if (boardController != null)
+            {
+                boardController.FinishedMiniGame(new[] {(boardController.players.Players[0], 1)});
+            }
+            else
+            {
+                InstantiatePlayer(0, 0);
+                InstantiateWall(1);
+                InstantiateWall(2);
+                InstantiateWall(3);
+            }
         }
         else if (maxPlayerNumber == 2)
         {
@@ -185,7 +195,7 @@ public class PongMain : MonoBehaviour
 
     public void PongBallDestroyed(int lastTouchedPlayer)
     {
-        Debug.Log("PongBallDestroyed called with lastTouchedPlayer: " + lastTouchedPlayer);
+//        Debug.Log("PongBallDestroyed called with lastTouchedPlayer: " + lastTouchedPlayer);
         if (lastTouchedPlayer != -1)
         {
             if (scores.TryGetValue(lastTouchedPlayer, out var currentScore))
