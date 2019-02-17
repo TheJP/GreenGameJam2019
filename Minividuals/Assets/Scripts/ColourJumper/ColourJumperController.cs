6a87b4dd10632754cc4dead6fbcbb2a34bc3176c
TheJP <jp_55555@hotmail.com>
2019-02-17 12:22:46 +0100

    Working on colour jumper game

--- Minividuals/Assets/Scripts/ColourJumper/ColourJumperController.cs
+++ Minividuals/Assets/Scripts/ColourJumper/ColourJumperController.cs
@@ -1,6 +1,9 @@
 ﻿using Assets.Scripts.Board;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColourJumperController : MonoBehaviour
 {
@@ -13,15 +16,34 @@ public class ColourJumperController : MonoBehaviour
     [Tooltip("Player parent GameObject")]
     public Transform playerParent;
 
+    [Tooltip("Textbox that allows information to be displayed")]
+    public Text displayText;
+
+    [Tooltip("Time during which colours are shuffled")]
+    public float colourShuffleTime = 3f;
+
+    [Tooltip("Time that the shuffler waits between multiple shuffles")]
+    public float shuffleWait = 0.2f;
+
+    [Tooltip("Time the players get to reach their platform at the start")]
+    public float initialRoundTime = 20f;
+
+    [Tooltip("Time by which the round time is reduced each round")]
+    public float roundTimeCutoff = 2f;
+
     /// <summary>Board controller used to interface with the rest of the games. May be null!</summary>
     private BoardController boardController;
 
     private List<ColourJumperPlayer> blobs;
 
+    private Platform[] platforms;
+
     private void Awake() => boardController = FindObjectOfType<BoardController>();
 
-    private void Start()
+    private IEnumerator Start()
     {
+        platforms = FindObjectsOfType<Platform>();
+
         var players = boardController?.players?.Players ??
             new[] { new Player(Color.green, "Player1_"), new Player(Color.blue, "Player2_") };
 
@@ -34,10 +56,34 @@ public class ColourJumperController : MonoBehaviour
             blobs.Add(blob);
             blob.Setup(player);
         }
+
+        yield return GameLoop();
     }
 
-    private void Update()
-    {
+    private bool MissingAColour() =>
+        blobs.Any(blob => !platforms.Any(platform => blob.Player.Colour == platform.Colour));
 
+    private void ShufflePlatformColours()
+    {
+        Debug.Assert(blobs.Count > 0);
+        foreach(var platform in platforms)
+        {
+            platform.SetColour(blobs[Random.Range(0, blobs.Count)].Player.Colour);
+        }
+    }
+
+    private IEnumerator GameLoop()
+    {
+        while (true)
+        {
+            var roundTime = initialRoundTime;
+            var startShuffle = Time.time;
+            while (Time.time - startShuffle < colourShuffleTime || MissingAColour())
+            {
+                ShufflePlatformColours();
+                yield return new WaitForSeconds(shuffleWait);
+            }
+            yield return new WaitForSeconds(3f);
+        }
     }
 }
---
+++ Minividuals/Assets/Scripts/ColourJumper/Platform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [Tooltip("Renderer used to give the platform a colour tint")]
+    public SpriteRenderer colourRenderer;
+
+    public Color Colour { get; private set; }
+
+    public void SetColour(Color colour)
+    {
+        Colour = colour;
+        colourRenderer.color = colour;
+    }
+}
